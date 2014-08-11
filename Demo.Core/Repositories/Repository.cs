0c0307b3338f699d0cf15edfdc5024932e8ed4c7
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-08-11 19:53:52 +0100

    Add a base repository

---
+++ Demo.Core/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿namespace Demo.Core.Repositories
+{
+    using System.Collections.Generic;
+    using System.Data.Entity;
+
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+
+        void SaveContext();
+    }
+
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext context;
+
+        protected Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        protected DbSet<T> Entities
+        {
+            get { return context.Set<T>(); }
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Entities;
+        }
+
+        public void Add(T entity)
+        {
+            Entities.Add(entity);
+        }
+
+        public void SaveContext()
+        {
+            context.SaveChanges();
+        }
+    }
+}
