54661db2b15be9668e2ca5d1133389f5f3beb9d7
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-08-11 19:25:48 +0100

    Add new blank web app.

---
+++ Demo.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Web;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Demo.Web
+{
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app) {}
+    }
+}
