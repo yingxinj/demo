ec3b7efedaa247d9132f2567e435ea83f0c0dc74
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-08-11 20:03:26 +0100

    Add Ninject

---
+++ Demo.Web/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,58 @@
+using Demo.Web;
+using WebActivatorEx;
+
+[assembly: PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: ApplicationShutdownMethod(typeof(NinjectWebCommon), "Stop")]
+
+namespace Demo.Web
+{
+    using Demo.Core;
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Extensions.Conventions;
+    using Ninject.Web.Common;
+    using System;
+    using System.Data.Entity;
+    using System.Web;
+
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper Bootstrapper = new Bootstrapper();
+
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            Bootstrapper.Initialize(CreateKernel);
+        }
+
+        public static void Stop()
+        {
+            Bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind(x => x.FromAssembliesMatching("Demo.*").SelectAllClasses().BindDefaultInterface());
+            kernel.Bind<DbContext>().To<DemoContext>().InRequestScope();
+        }
+    }
+}
